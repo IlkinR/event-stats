f08590c1092e751ce66e953318432c758568b5e2
Ilkin Ramazanov <ilkin.ramazanov@gmail.com>
2021-12-15 01:39:06 +0400

    replaced services part with fetching all events

--- source/stats/views.py
+++ source/stats/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 
 from .models import EventStats
 from .serializers import ListEventStatsSerializer, EventStatsSerializer
-from .services import get_sorted_event_stats, delete_all_event_stats
+from .services import get_sorted_event_stats, delete_all_event_stats, get_all_event_stats
 
 AVAILABLE_EVENT_STATS_FIELDS = tuple(field.name for field in EventStats._meta.fields)
 
@@ -20,12 +20,14 @@ class ListEventsStatsView(ListAPIView):
     model = EventStats
 
     def get_queryset(self):
+        # Validating date queries
         start_date = self.request.query_params.get('from')
         last_date = self.request.query_params.get('to')
         if to_date(start_date) > to_date(start_date):
             fail_details = "Start date cann't be greater than end date!"
             raise exceptions.ParseError(fail_details)
 
+        # Validating field wchich will be used for sorting
         field_to_sort = self.request.query_params.get('sortway')
         if field_to_sort not in AVAILABLE_EVENT_STATS_FIELDS:
             right_fields_msg = ', '.join(AVAILABLE_EVENT_STATS_FIELDS)
@@ -36,12 +38,12 @@ class ListEventsStatsView(ListAPIView):
 
 
 class CreateEventStatsView(CreateAPIView):
-    queryset = EventStats.objects.all()
+    queryset = get_all_event_stats()
     serializer_class = EventStatsSerializer
 
 
 class DeleteEventStatsView(UpdateAPIView):
-    queryset = EventStats.objects.all()
+    queryset = get_all_event_stats()
     serializer_class = EventStatsSerializer
 
     def delete(self, request):
